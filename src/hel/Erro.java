f15b0af27695eb13b1741a0b2fa281506f8dc778
Guilherme Afonso <gasp@cin.ufpe.br>
2019-12-01 01:01:21 -0300

    Comments added

--- src/hel/Erro.java
+++ src/hel/Erro.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*Class that set up the error message form
+which has a label with the message and
+a OK button*/
+
 public class Erro {
     private JPanel panel1;
     private JButton OKButton;
     private JLabel msg;
 
     public Erro(String texto) {
+        
+        //Creates the window frame and set up all his elements
         JFrame janela = new JFrame();
         janela.setContentPane(panel1);
         janela.setVisible(true);
@@ -17,11 +23,16 @@ public class Erro {
         janela.setSize(300, 150);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         msg.setText(texto);
-
+        
+        //OK button listener
         OKButton.addActionListener(new ActionListener() {
+            
+            //Method executed when the button is pressed
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(e.getActionCommand().equals(OKButton.getActionCommand())){
+                    
+                    //Set not visible and close the GUI
                     janela.setVisible(false);
                     janela.dispose();
                 }

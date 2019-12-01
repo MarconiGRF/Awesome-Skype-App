32467e4cc9fd98bc46dfd46440bb4f6a4fdf66d3
Guilherme Afonso <gasp@cin.ufpe.br>
2019-11-30 23:31:59 -0300

    Clean and comment client 1 code

--- src/hel/Cliente1.java
+++ src/hel/Cliente1.java
@@ -7,45 +7,51 @@ import java.util.Scanner;
 
 public class Cliente1 {
 
-
     public static void main(String[] args) throws IOException {
-        try {
+        try {	
+	    	//Creates a object for all data needed for connection
         	ConnectionData data = new ConnectionData("","0.0.0.0", 0, 0, 1);
-			loginForm login = new loginForm(data);
-			while (data.name.equals("") || data.ip.equals("0.0.0.0") || data.serverPort == 0 || data.textPort == 0|| data.audioPort == 0) {
+		
+	    	//Creates the login form
+	    	loginForm login = new loginForm(data);
+		
+	    	//Stays on this loop until the data object is modified on the login form
+	    	while (data.name.equals("") || data.ip.equals("0.0.0.0") || data.serverPort == 0 || data.textPort == 0|| data.audioPort == 0) {
 				Thread.sleep(100);
-			}
+	    	}
+	
+			//Creates a stream socket and connects it to the specified port number at the specified IP address
             Socket clienteSocket = new Socket(data.ip, data.serverPort);
-
+			
+			//Creates a buffered character-output stream to the server that uses a default-sized output buffer.
             BufferedWriter writeToServer = new BufferedWriter(new OutputStreamWriter(clienteSocket.getOutputStream()));
+			
+			//Write two lines for the server, one containing the port and another containing the ip
             writeToServer.write(Integer.toString(data.textPort) + "\n");
             writeToServer.write(clienteSocket.getInetAddress().getHostAddress() + "\n");
             writeToServer.flush();
 
-
-            System.out.println("Socket iniciado com o servidor.");
-
-
+			//Creates a buffering character-input stream to the server that uses a default-sized input buffer.
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
-            System.out.println("Buffer lido do servidor.");
+
+			/*Reads two lines of text coming from server
+			ont the port and the other the ip of the another client*/
             int portaCliente2 = Integer.parseInt(inFromServer.readLine());
             String ipCliente2 = inFromServer.readLine();
 
-            System.out.println("Porta do outro cliente: " + portaCliente2);
-            System.out.println("Messagem lida do buffer");
+			//Creates the object node which make the connection p2p between clients
             Node u1 = new Node(clienteSocket.getInetAddress(), data.textPort, portaCliente2, data.name);
-            System.out.println("Conexao criada com outro cliente");
 
-            //loginForm login = new loginForm();
+			//Creates the client form
             gui helgui = new gui(u1, portaCliente2, ipCliente2, clienteSocket);
+			
+			//Creates the Thread which the client will be receiving messages
             Thread r1 = new Receber(u1, 1, helgui);
             r1.start();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
-
 }
 
 
